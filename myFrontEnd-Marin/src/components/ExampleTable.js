ad64e456b7e9f04ddb9ac6005fbf294ea155a5df
Marin Alquinga Josue Isaac <jimarin@espe.edu.ec>
2025-03-08 22:31:58 -0500

    Enhance ExampleTable component with Bootstrap styling for improved UI

--- myFrontEnd-Marin/src/components/ExampleTable.js
+++ myFrontEnd-Marin/src/components/ExampleTable.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { API_BASE_URL } from '../config';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
 const ExampleTable = () => {
   const [users, setUsers] = useState([]);
@@ -12,36 +13,38 @@ const ExampleTable = () => {
   }, []);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Cedula</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Address</th>
-          <th>Phone</th>
-          <th>Email</th>
-          <th>Gender</th>
-          <th>Role</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map(user => (
-          <tr key={user.id}>
-            <td>{user.id}</td>
-            <td>{user.cedula}</td>
-            <td>{user.first_name}</td>
-            <td>{user.last_name}</td>
-            <td>{user.address}</td>
-            <td>{user.phone}</td>
-            <td>{user.email}</td>
-            <td>{user.gender}</td>
-            <td>{user.role}</td>
+    <div className="container mt-5">
+      <table className="table table-striped table-bordered">
+        <thead className="thead-dark">
+          <tr>
+            <th>ID</th>
+            <th>Cedula</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Role</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.cedula}</td>
+              <td>{user.first_name}</td>
+              <td>{user.last_name}</td>
+              <td>{user.address}</td>
+              <td>{user.phone}</td>
+              <td>{user.email}</td>
+              <td>{user.gender}</td>
+              <td>{user.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
