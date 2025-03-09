0691fd5b4789f546e3635da7dc3b7e995c8faab7
Marin Alquinga Josue Isaac <jimarin@espe.edu.ec>
2025-03-08 22:07:41 -0500

    first commit

---
+++ myFrontEnd-Marin/src/components/ExampleTable.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { API_BASE_URL } from '../config';
+
+const ExampleTable = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API_BASE_URL}/api/users`)
+      .then(response => response.json())
+      .then(data => setUsers(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Cedula</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Address</th>
+          <th>Phone</th>
+          <th>Email</th>
+          <th>Gender</th>
+          <th>Role</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map(user => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.cedula}</td>
+            <td>{user.first_name}</td>
+            <td>{user.last_name}</td>
+            <td>{user.address}</td>
+            <td>{user.phone}</td>
+            <td>{user.email}</td>
+            <td>{user.gender}</td>
+            <td>{user.role}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default ExampleTable;
---
+++ myFrontEnd-Marin/src/config.js
@@ -0,0 +1,3 @@
+export const API_BASE_URL = 'https://hollister-center.vercel.app';
+
+// export const API_BASE_URL = 'http://localhost:3011';
